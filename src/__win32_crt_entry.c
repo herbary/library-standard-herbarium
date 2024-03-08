27ce010cf5b63648e3873ce77c41a6593e3741c2
herbary <149976203+herbary@users.noreply.github.com>
2024-03-08 15:35:44 +0300

    Add /D flags for WIN32_LEAN_AND_MEAN & WIN32_EXTRA_LEAN

--- src/__win32_crt_entry.c
+++ src/__win32_crt_entry.c
@@ -5,7 +5,6 @@
 #pragma comment(lib, "kernel32.lib")
 #endif // _MSC_VER
 
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 #include "herbary/std/define/_Noreturn.h" // IWYU pragma: keep
--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 #pragma comment(lib, "kernel32.lib")
 #endif // _MSC_VER
 
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <tchar.h>
 

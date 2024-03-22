9d5c74d8a1306ceb9fa70f007d70457dae61d94f
herbary <149976203+herbary@users.noreply.github.com>
2024-03-22 16:57:15 +0300

    Align return type pointer positions in __win32_crt_memory.h & *.c

--- src/__win32_crt_memory.h
+++ src/__win32_crt_memory.h
@@ -4,8 +4,8 @@
 
 #include <stddef.h>
 
-void * memset(void *dest, int c, size_t count);
-void * memcpy(void *dest, const void *src, size_t count);
+void* memset(void *dest, int c, size_t count);
+void* memcpy(void *dest, const void *src, size_t count);
 
 #endif // HERBARY_STD___WIN32_CRT_MEMORY_H_INCLUDED
 #endif // _WIN32

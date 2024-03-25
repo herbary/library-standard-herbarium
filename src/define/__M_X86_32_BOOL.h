befcb4b8742673b1cf49b0b71596dc947409decf
herbary <149976203+herbary@users.noreply.github.com>
2024-03-25 17:07:14 +0300

    Fix typo in __M_X86_32_BOOL guard error

--- src/define/__M_X86_32_BOOL.h
+++ src/define/__M_X86_32_BOOL.h
@@ -2,7 +2,7 @@
 #define HERBARY_STD_DEFINE___M_X86_32_BOOL_H_INCLUDED
 
 #ifdef __M_X86_32_BOOL
-#error "`__M_X86_32_BOOL` mustb't be pre-defined"
+#error "`__M_X86_32_BOOL` mustn't be pre-defined"
 #endif // __M_X86_32_BOOL
 
 #include "herbary/std/define/__M_X86_32.h" // IWYU pragma: keep
@@ -12,6 +12,5 @@
 #else
 #define __M_X86_32_BOOL 0
 #endif // __M_X86_32
-
 
 #endif // HERBARY_STD_DEFINE___M_X86_32_BOOL_H_INCLUDED
